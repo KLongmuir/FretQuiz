34613f038116e1c0baaae0dd408c5a888641b3e2
Longmuir <longmuirk@BKSQ-G2VV-MAC.local>
2017-07-26 11:20:17 -0400

    quiz is now the minimum functioning product. game is playable

--- FretQuiz/Lines.swift
+++ FretQuiz/Lines.swift
@@ -15,6 +15,13 @@ class Lines {
     var screenWidth = 0
     var screenHeight = 0
     let fretWidth = 67
+    
+    let stringDict: [String:Int] = ["E": 326, "A": 251, "D": 176, "G": 101]
+    
+    var fretDict: [String:Int] = [:]
+    
+    
+    
     // 976 is the space I have width wise
     // margin of 24 pixels on each side
     // need 13 frets, the first creating space for the open string
@@ -29,6 +36,24 @@ class Lines {
         print(screenWidth)
         screenHeight = Int(screenDimensions.height)
         print(screenHeight)
+        fretDict = ["0": (44 + fretWidth), "1": (46 + (fretWidth*2)), "2": (46 + (fretWidth*3)), "3": (46 + (fretWidth*4)), "4": (46 + (fretWidth*5)), "5": (46 + (fretWidth*6)), "6": (46 + (fretWidth*7)), "7": (46 + (fretWidth*8)), "8": (46 + (fretWidth*9)), "9": (46 + (fretWidth*10)), "10": (46 + (fretWidth*11)), "11": (46 + (fretWidth*12)), "12": (46 + (fretWidth*13))]
+    }
+    
+    func generateCircle(stringName: String) -> CAShapeLayer {
+        var stringSplit = stringName.components(separatedBy: ":")
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: fretDict[stringSplit[1]]!,y: stringDict[stringSplit[0]]!), radius: CGFloat(6), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.red.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = 3.0
+        
+        return shapeLayer
     }
     
     
--- FretQuiz/ViewController.swift
+++ FretQuiz/ViewController.swift
@@ -39,6 +39,13 @@ class ViewController: UIViewController {
     // CONSTANTS
     let priority = DispatchQoS.QoSClass.default
     let screenSize: CGRect = UIScreen.main.bounds
+    let possibleNotes: Array<String> = ["A♭","A","A♯","B♭","B","C","C♯","D♭","D","D♯","E♭","E","E♯","F","F♯","G♭","G","G#"]
+    let possibleFrets: Array<String> = ["E:0", "E:1", "E:2", "E:3", "E:4", "E:5", "E:6", "E:7", "E:8", "E:9", "E:10", "E:11", "E:12", "A:0", "A:1", "A:2", "A:3", "A:4", "A:5", "A:6", "A:7", "A:8", "A:9", "A:10", "A:11", "A:12", "D:0", "D:1", "D:2", "D:3", "D:4", "D:5", "D:6", "D:7", "D:8", "D:9", "D:10", "D:11", "D:12", "G:0", "G:1", "G:2", "G:3", "G:4", "G:5", "G:6", "G:7", "G:8", "G:9", "G:10", "G:11", "G:12"]
+    
+    let fretsToNotes: [String:Array<String>] = ["E:0": ["E"], "E:1": ["F"], "E:2": ["F♯"], "E:3": ["G"], "E:4": ["G♯", "A♭"], "E:5": ["A"], "E:6": ["A♯", "B♭"], "E:7": ["B"], "E:8": ["C"], "E:9": ["C♯","D♭"], "E:10": ["D"], "E:11": ["D♯","E♭"], "E:12": ["E"], "A:0": ["A"], "A:1": ["A♯", "B♭"], "A:2": ["B"], "A:3": ["C"], "A:4": ["C"], "A:5": ["D"], "A:6": ["D♯","E♭"], "A:7": ["E"], "A:8": ["F"], "A:9": ["F♯","G♭"], "A:10": ["G"], "A:11": ["G♯", "A♭"], "A:12": ["A"], "D:0": ["D"], "D:1": ["D♯","E♭"], "D:2": ["E"], "D:3": ["F"], "D:4": ["F♯","G♭"], "D:5": ["G"], "D:6": ["G♯", "A♭"], "D:7": ["A"], "D:8": ["A♯", "B♭"], "D:9": ["B"], "D:10": ["C"], "D:11": ["C♯","D♭"], "D:12": ["D"], "G:0": ["G"], "G:1": ["G♯", "A♭"], "G:2": ["A"], "G:3": ["A♯", "B♭"], "G:4": ["B"], "G:5": ["C"], "G:6": ["C♯","D♭"], "G:7": ["D"], "G:8": ["D♯","E♭"], "G:9": ["E"], "G:10": ["F"], "G:11": ["F♯","G♭"], "G:12": ["G"]]
+    
+    let lineMaker = Lines(screenDimensions: UIScreen.main.bounds)
+    
     
     // VARIABLES
     var instrStrings: Array<UIView> = []
@@ -46,11 +53,13 @@ class ViewController: UIViewController {
     var noteButtons: Array<UIButton> = []
     var isPlaying = false
     var points = 0
+    var dotReference: CALayer = CALayer()
+    var dotString: String = ""
     
     // COLORS
     let myGreen = UIColor(red: 103/255.0, green: 178/255.0, blue: 104/255.0, alpha: 1.0)
     
-    let possibleNotes: Array<String> = ["A♭","A","A♯","B♭","B","C","C♯","D♭","D","D♯","E♭","E","E♯","F","F♯","G♭","G","G#"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,7 +80,7 @@ class ViewController: UIViewController {
         playButton.layer.borderWidth = 2
         playButton.layer.borderColor = myGreen.cgColor
         playButton.setTitleColor(UIColor.white, for: UIControlState.normal)
-
+        
         makeFretboard()
     }
 
@@ -81,7 +90,6 @@ class ViewController: UIViewController {
     }
     
     func makeFretboard(){
-        let lineMaker = Lines(screenDimensions: screenSize)
         instrStrings.append(lineMaker.generateHorizontalLine(stringName: "E"))
         instrStrings.append(lineMaker.generateHorizontalLine(stringName: "A"))
         instrStrings.append(lineMaker.generateHorizontalLine(stringName: "D"))
@@ -130,21 +138,21 @@ class ViewController: UIViewController {
         noteButtons.append(GFlatButton)
         noteButtons.append(GButton)
         noteButtons.append(GSharpButton)
-
+        
     }
     
     func newNote(){
         if isPlaying == true{
-            self.noteName.text = self.possibleNotes[Int(arc4random_uniform(UInt32(self.possibleNotes.count - 1)))]
+            dotString = self.possibleFrets[Int(arc4random_uniform(UInt32(self.possibleFrets.count - 1)))]
+            dotReference = lineMaker.generateCircle(stringName: dotString)
+            view.layer.addSublayer(dotReference)
         }
     }
     
     @IBAction func buttonPressed(_ sender: UIButton){
-        FButton.backgroundColor = .clear
-        FButton.layer.cornerRadius = 20
-        FButton.layer.borderWidth = 1
-        FButton.layer.borderColor = UIColor.black.cgColor
-        if self.noteName.text == sender.titleLabel?.text {
+        print(dotString)
+        if (self.fretsToNotes[dotString]?.contains((sender.titleLabel?.text)!))! {
+            dotReference.removeFromSuperlayer()
             self.rightWrong.text = "Correct"
             self.rightWrong.textColor = myGreen
             points += 1
